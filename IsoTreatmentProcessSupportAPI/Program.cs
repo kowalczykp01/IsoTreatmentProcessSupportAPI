63e3880f8aad5aa78a224ce03ae5562bc80ec126
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2023-09-08 00:03:01 +0200

    Added nlog and ErrorHandlingMiddleware

--- IsoTreatmentProcessSupportAPI/Program.cs
+++ IsoTreatmentProcessSupportAPI/Program.cs
@@ -1,9 +1,13 @@
 using IsoTreatmentProcessSupportAPI;
 using IsoTreatmentProcessSupportAPI.Entities;
+using IsoTreatmentProcessSupportAPI.Middlewares;
 using IsoTreatmentProcessSupportAPI.Services;
 using Microsoft.AspNetCore.Identity;
+using NLog.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseNLog();
 
 // Add services to the container.
 var authenticationSettings = new AuthenticationSettings();
@@ -17,10 +21,13 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddTransient<IMailkitService, MailkitService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 

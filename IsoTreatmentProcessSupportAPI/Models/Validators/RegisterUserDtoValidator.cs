ac06e8360c51873d730e636df9ca1aa004ad9f70
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2023-09-12 21:22:47 +0200

    Added validation for register

---
+++ IsoTreatmentProcessSupportAPI/Models/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using IsoTreatmentProcessSupportAPI.Entities;
+
+namespace IsoTreatmentProcessSupportAPI.Models.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator(IsoSupportDbContext dbContext)
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty();
+
+            RuleFor(x => x.LastName)
+                .NotEmpty();
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.ConfirmPassword).Equal(e => e.Password).WithMessage("Passwords must match");
+
+            RuleFor(x => x.Email)
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dbContext.Users.Any(u => u.Email == value);
+                    if (emailInUse)
+                    {
+                        context.AddFailure("Email", "This email is taken");
+                    }
+                });
+        }
+    }
+}

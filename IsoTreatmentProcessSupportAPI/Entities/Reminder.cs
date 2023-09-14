0ae8a9611638db3f8f7e1564e24c5c6d69c5d9aa
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2023-09-14 19:45:55 +0200

    Removed IsTaken property and changed type of Time property to TimeOnly

--- IsoTreatmentProcessSupportAPI/Entities/Reminder.cs
+++ IsoTreatmentProcessSupportAPI/Entities/Reminder.cs
@@ -3,8 +3,7 @@
     public class Reminder
     {
         public int Id { get; set; }
-        public DateTime Time { get; set; }
-        public bool IsTaken { get; set; }
+        public TimeOnly Time { get; set; }
         public int UserId { get; set; }
         public virtual User User { get; set; }
     }

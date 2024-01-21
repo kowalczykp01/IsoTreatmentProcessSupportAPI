3820636f952a4999eab4035900691fce0041250b
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2024-01-22 00:44:41 +0100

    Change in claim - instead of firstname and surname there is only firstname in claim

--- IsoTreatmentProcessSupportAPI/Services/UserService.cs
+++ IsoTreatmentProcessSupportAPI/Services/UserService.cs
@@ -123,7 +123,7 @@ namespace IsoTreatmentProcessSupportAPI.Services
             var claims = new List<Claim>()
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
+                new Claim(ClaimTypes.Name, $"{user.FirstName}"),
                 new Claim(ClaimTypes.Email, $"{user.Email}")
             };
 
